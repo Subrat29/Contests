e2dbdabf634d573f0db7c427613bcdb1db5812e7
Subrat29 <ankubanku29062003@gmail.com>
2024-04-07 07:55:34 +0530

    Biweekly 127

--- Biweekly Contest 127/100250. Minimum Levels to Gain More Points/sol1.cpp
+++ Biweekly Contest 127/100250. Minimum Levels to Gain More Points/sol1.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits.h>
 using namespace std;
 
-
+int minimumSubarrayLength(vector<int> &nums, int k)
+{
+    int minLen = INT_MAX;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int OR = 0;
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            OR = OR | nums[j];
+            if (OR >= k)
+                minLen = min(j - i + 1, minLen);
+        }
+    }
+    if (minLen != INT_MAX)
+        return minLen;
+    return -1;
+}
 
 int main()
 {
---
+++ Biweekly Contest 127/3096. Minimum Levels to Gain More Points/sol1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int minimumLevels(vector<int> &possible)
+{
+}
+
+int main()
+{
+    return 0;
+}
