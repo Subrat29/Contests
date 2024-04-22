d0f66f856852cc92faffa1e7175bd0ca2b671bd1
Subrat29 <ankubanku29062003@gmail.com>
2024-04-22 23:24:50 +0530

    gcd tip

---
+++ aLearnings/Gcd/gcd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// We know that product of A and B is divisible by a number B if GCD(A, B) = B.
+
+int main()
+{
+    return 0;
+}
